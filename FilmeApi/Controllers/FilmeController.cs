32439d746fd77d4b18585edefc0c9dfc21fc3dd7
wagner <you@example.com>
2022-10-31 08:43:09 -0300

    primeiro commit

---
+++ FilmeApi/Controllers/FilmeController.cs
@@ -0,0 +1,42 @@
+﻿using FilmeApi.Model;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace FilmeApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public IActionResult AdicionaFlme([FromBody] Filme filme)
+        {
+            filme.id = id++;
+            filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = filme.id }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperaFilmes()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmesPorId(int id)
+        {
+           Filme filme = filmes.FirstOrDefault(filme => filme.id == id);
+            if(filme != null)
+            {
+                return Ok(filme);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ FilmeApi/Model/Filme.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmeApi.Model
+{
+    public class Filme
+    {
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "O Campo Título é Obrigatório")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O Campo Diretor é Obrigatório")]
+        public string Diretor { get; set; }
+
+        public string Genero { get; set; }
+
+        [Range(1,600, ErrorMessage = " A duração deve ter no mínimo 1 no máximo 600 minutos")]
+        public int Duracao { get; set; }
+
+    }
+}
