02a1db4aeffd64ec831b598881d8b6f6bcef010b
wagner <you@example.com>
2023-01-08 15:36:57 -0300

    22 commit

--- UsuariosApi/Program.cs
+++ UsuariosApi/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddDbContext<UserDbContext>(opts => opts.UseLazyLoadingProxies().UseMySql(builder.Configuration.GetConnectionString("UsuarioConnection"), new MySqlServerVersion(new Version(8, 0))));
 builder.Services.AddIdentity<IdentityUser<int>, IdentityRole<int>>().AddEntityFrameworkStores<UserDbContext>();
 builder.Services.AddScoped<CadastroService, CadastroService>();
+builder.Services.AddScoped<LoginService, LoginService>();
 
 
 var app = builder.Build();
